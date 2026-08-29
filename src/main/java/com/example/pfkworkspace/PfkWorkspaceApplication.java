3c3686a09323ebb9e68be3217bf1b4cc022f5539
Nuh Ali <noah@pfkdigital.co.uk>
2026-08-29 06:26:58 +0100

    feat: wire up spring redis cache configuration and bean

--- src/main/java/com/example/pfkworkspace/PfkWorkspaceApplication.java
+++ src/main/java/com/example/pfkworkspace/PfkWorkspaceApplication.java
@@ -2,8 +2,10 @@ package com.example.pfkworkspace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class PfkWorkspaceApplication {
     public static void main(String[] args) {
         SpringApplication.run(PfkWorkspaceApplication.class, args);
---
+++ src/main/java/com/example/pfkworkspace/config/RedisConfig.java
@@ -0,0 +1,33 @@
+package com.example.pfkworkspace.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+
+@Configuration
+public class RedisConfig {
+
+  @Bean
+  public RedisCacheManager redisCacheManager(RedisConnectionFactory redisConnectionFactory) {
+    RedisCacheConfiguration defaultCacheConfig =
+        RedisCacheConfiguration.defaultCacheConfig()
+            .entryTtl(Duration.ofMinutes(10))
+            .disableCachingNullValues()
+            .serializeKeysWith(
+                RedisSerializationContext.SerializationPair.fromSerializer(
+                    new StringRedisSerializer()))
+            .serializeValuesWith(
+                RedisSerializationContext.SerializationPair.fromSerializer(
+                    new GenericJackson2JsonRedisSerializer()));
+    return RedisCacheManager.builder(redisConnectionFactory)
+        .cacheDefaults(defaultCacheConfig)
+        .build();
+  }
+}
